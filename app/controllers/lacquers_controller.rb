9d75b2f26514ad92bd9b196b6b4b584b0d2bbabf
Rose Weixel <rose.weixel@gmail.com>
2014-11-27 12:43:54 -0500

    start working on dropdown

--- app/controllers/lacquers_controller.rb
+++ app/controllers/lacquers_controller.rb
@@ -1,5 +1,9 @@
 class LacquersController < ApplicationController
   def index
     @lacquers = Lacquer.all
+    @opi_laquers = Lacquer.where(brand: "OPI").first
+    @essie_laquers = Lacquer.where(brand: "Essie").first
+    @butter_laquers = Lacquer.where(brand: "Butter London").first
+    @deborah_laquers = Lacquer.where(brand: "Deborah Lippmann").first
   end
 end
