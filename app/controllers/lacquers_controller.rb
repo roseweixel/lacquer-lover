c94210221a74439b25d8f97a4405d05c72abedd6
Rose Weixel <rose.weixel@gmail.com>
2014-11-27 22:03:33 -0500

    do more work on dropdown

--- app/controllers/lacquers_controller.rb
+++ app/controllers/lacquers_controller.rb
@@ -1,9 +1,9 @@
 class LacquersController < ApplicationController
   def index
-    @lacquers = Lacquer.all
-    @opi_laquers = Lacquer.where(brand: "OPI").first
-    @essie_laquers = Lacquer.where(brand: "Essie").first
-    @butter_laquers = Lacquer.where(brand: "Butter London").first
-    @deborah_laquers = Lacquer.where(brand: "Deborah Lippmann").first
+    @lacquer = Lacquer.new
+    @opi_lacquers = Lacquer.where(brand: "OPI").first
+    @essie_lacquers = Lacquer.where(brand: "Essie").first
+    @butter_lacquers = Lacquer.where(brand: "Butter London").first
+    @deborah_lacquers = Lacquer.where(brand: "Deborah Lippmann").first
   end
 end
