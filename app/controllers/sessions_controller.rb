bbc8e082de6fcf908c8ba43af56d1c0e750379f5
Rose Weixel <rose.weixel@gmail.com>
2014-11-28 03:23:06 -0500

    start adding non-facebook sigin/signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,20 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to root_path
+    if params[:username].present?
+      user = User.find_by(:username => params[:username])
+      if user
+        session[:user_id] = user.id
+        redirect_to root_path
+      else
+        flash[:notice] = "Could not find that person, sorry!"
+        redirect_to root_path
+      end
+    elsif request.env['omniauth.auth'].present?
+      user = User.from_omniauth(env["omniauth.auth"])
+      session[:user_id] = user.id
+      redirect_to root_path
+    end
+    
   end
 
   def destroy
