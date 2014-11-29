b506a64c3a6502315874cd76b41eaab214113bc1
Rose Weixel <rose.weixel@gmail.com>
2014-11-29 16:29:32 -0500

    add lacquer dropdown to user showpage if current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,12 @@ class UsersController < ApplicationController
   end
 
   def show
+    @new_lacquer = Lacquer.new
+    @new_user_lacquer = UserLacquer.new
+    @opi_lacquers = Brand.where(name: "OPI").first.lacquers
+    @essie_lacquers = Brand.where(name: "Essie").first.lacquers
+    @butter_lacquers = Brand.where(name: "Butter London").first.lacquers
+    @deborah_lacquers = Brand.where(name: "Deborah Lippmann").first.lacquers
     @user = User.find(params[:id])
     @friendship = current_user.friendships.new(friend: @user)
     @transaction = Transaction.new
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,9 +4,7 @@ class Transaction < ActiveRecord::Base
 
   validate :transaction_must_be_unique, :on => :create
   
-  # before_save do
-  #   self.owner_id = username.gsub(" ", "-").downcase
-  # end
+  SECONDS_PER_DAY = 86400
 
   def owner
     User.find(user_lacquer.user_id)
@@ -15,6 +13,24 @@ class Transaction < ActiveRecord::Base
   def transaction_must_be_unique
     if !Transaction.where(:user_lacquer_id => user_lacquer_id, :requester_id => requester_id, :state => ['pending', 'accepted']).empty?
       errors.add(:transaction, "This request already exists!")
+    end
+  end
+
+  def due_today?
+    due_date == Date.today
+  end
+
+  def due_tomorrow?
+    due_date - 1 == Date.today
+  end
+
+  def overdue?
+    due_date < Date.today
+  end
+
+  def days_overdue
+    if overdue?
+      Date.today.to_time.to_i / SECONDS_PER_DAY - Transaction.last.due_date.to_time.to_i / SECONDS_PER_DAY
     end
   end
 end
