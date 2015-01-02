31c8e50c92a6ffb7b81ee8272f9e6ea95ebb3808
Rose Weixel <rose.weixel@gmail.com>
2015-01-01 22:01:50 -0500

    finish users/authentication tests

--- spec/factories/brands.rb
+++ spec/factories/brands.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
-  factory :brand do
-    
+  sequence :brand_name do |n|
+    "Brand#{n}"
   end
 
+  factory :brand do
+    name { generate(:brand_name) }
+  end
 end
+
--- spec/features/users_and_authentication_spec.rb
+++ spec/features/users_and_authentication_spec.rb
@@ -4,11 +4,15 @@ include IntegrationSpecHelper
 
 context 'when visiting the home page' do
   before(:each) do
+    @brand1 = create(:brand, name: "OPI")
+    @brand2 = create(:brand, name: "Essie")
+    @brand3 = create(:brand, name: "Butter London")
+    @brand4 = create(:brand, name: "Deborah Lippmann")
+    @other_user = create(:user, name: "Nancy Nails")
     visit(root_path)
   end
 
   it 'responds with 200' do
-    #save_and_open_page
     expect(page.status_code).to be(200)
   end
 
@@ -17,8 +21,26 @@ context 'when visiting the home page' do
       login_with_oauth
     end
 
-    it "shows a link to the current user's profile page" do
+    it "shows a link to the signed in user's profile page" do
       expect(page).to have_content('Welcome, Lucy! See your profile.')
+    end
+
+    context 'when visiting a user profile' do
+      it "redirects to the signed in user's profile when profile link is clicked" do
+        click_link('Welcome, Lucy! See your profile.')
+        expect(page).to have_content('Your Profile')
+        expect(page).to have_content('Your Lacquers')
+        expect(page).to have_content('Add A Lacquer')
+      end
+
+      it "shows the appropriate profile page when navigating to another user's profile" do
+        
+        visit('users/1')
+        
+        expect(page).to have_content("Nancy's Profile")
+        expect(page).to_not have_content("Add A Lacquer")
+        expect(page).to_not have_content("Remove From Your Collection")
+      end
     end
 
     it "has a sign out link that signs the user out" do
