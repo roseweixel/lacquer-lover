d135681bf184c5c595186abe111c6596c1f3ca22
molgin <molgin@users.noreply.github.com>
2015-02-12 22:50:29 -0500

    add user and lacquer associations to Transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,13 +1,15 @@
 class Transaction < ActiveRecord::Base
   belongs_to :user_lacquer
+  delegate :lacquer, to: :user_lacquer
+  delegate :user, to: :user_lacquer
   belongs_to :requester, class_name: 'User', foreign_key: 'requester_id'
   #belongs_to :user, through: :user_lacquer, dependent: :destroy
   before_create :defaults
 
-  validate :transaction_must_be_unique, :on => :create
-  validate :user_lacquer_must_be_loanable, :on => :create
-  validate :user_lacquer_must_not_be_on_loan, :on => :create
-  validate :requester_and_user_must_be_friends, :on => :create
+  # validate :transaction_must_be_unique, :on => :create
+  # validate :user_lacquer_must_be_loanable, :on => :create
+  # validate :user_lacquer_must_not_be_on_loan, :on => :create
+  # validate :requester_and_user_must_be_friends, :on => :create
 
   SECONDS_PER_DAY = 86400
 
