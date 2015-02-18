ddd030ac96076c841238163210123452ad1dc1a8
molgin <molgin@users.noreply.github.com>
2015-02-17 23:14:13 -0500

    add some basic validations

--- app/models/color.rb
+++ app/models/color.rb
@@ -2,4 +2,6 @@ class Color < ActiveRecord::Base
   has_many :user_lacquer_colors
   has_many :user_lacquers, through: :user_lacquer_colors
   has_many :lacquers, through: :user_lacquers
+
+  validates_presence_of :name
 end
--- app/models/finish.rb
+++ app/models/finish.rb
@@ -2,4 +2,6 @@ class Finish < ActiveRecord::Base
   has_many :user_lacquer_finishes
   has_many :user_lacquers, through: :user_lacquer_finishes
   has_many :lacquers, through: :user_lacquers
+
+  validates_presence_of :description
 end
