bc65924f3fba382057ce54fca2c7cf46c072a0f1
Rose Weixel <rose.weixel@gmail.com>
2015-02-23 17:32:28 -0500

    redirect to profile page on signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
     if request.env['omniauth.auth'].present?
       user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = user.id
-      redirect_to root_path
+      redirect_to user_path(user)
     elsif params[:user][:name].present?
       user = User.find_by(:name => params[:user][:name]) || User.create(:name => params[:user][:name])
       if user
         session[:user_id] = user.id
-        redirect_to root_path
+        redirect_to user_path(user)
       else
         flash[:notice] = "Could not find that person, sorry!"
         redirect_to root_path
