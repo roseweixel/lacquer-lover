2a76777e10694ba8d4a1d2490281a8201e6fa248
Rose Weixel <rose.weixel@gmail.com>
2015-03-07 17:46:46 -0500

    add image from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,8 @@ class User < ActiveRecord::Base
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
+      user.image = auth.info.image
+      user.save!
     end
   end
 
