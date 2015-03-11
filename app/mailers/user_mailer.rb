7180dc968b26247270ad2ffd7787ed659e984de6
molgin <molgin@users.noreply.github.com>
2015-03-10 20:39:55 -0400

    create user mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ActionMailer::Base
+  default from: "Lacquer Love&Lend <noreply@lacquer-love-and-lend.herokuapp.com>"
+
+  # Emails to send:
+
+  # welcome on first login
+  # notif of loan request
+  # notif of loan request accepted
+  # notif of loan turned into gift?
+  # notif of friend request
+  # notif of friend request accepted
+
+end
