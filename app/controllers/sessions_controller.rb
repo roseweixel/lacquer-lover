6445dbcfd51c05ba0915790bc52ecbf6852926be
Rose Weixel <rose.weixel@gmail.com>
2015-03-12 22:42:16 -0400

    when signing in route to where you came from unless signing in from root_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,12 @@ class SessionsController < ApplicationController
     if request.env['omniauth.auth'].present?
       user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = user.id
-      redirect_to user_path(user)
+      #binding.pry
+      if request.env["HTTP_REFERER"].gsub(request.base_url, "") == root_path
+        redirect_to user_path(user)
+      else
+        redirect_to request.env["HTTP_REFERER"].gsub(request.base_url, "")
+      end
     elsif params[:user][:name].present?
       user = User.find_by(:name => params[:user][:name]) || User.create(:name => params[:user][:name])
       if user
