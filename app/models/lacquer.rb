7320850f9c910fca4a8e251fca9c4d06a3398c5c
Rose Weixel <rose.weixel@gmail.com>
2015-03-23 17:45:36 -0400

    create levenstein distance on Lacquer model based on name attribute

--- app/models/lacquer.rb
+++ app/models/lacquer.rb
@@ -80,6 +80,44 @@ class Lacquer < ActiveRecord::Base
     lacquers_matching_all_words
   end
 
+  def levenshtein_distance(string)
+    string.downcase!
+    text = name.downcase
+    m = string.length
+    n = text.length
+    return m if n == 0
+    return n if m == 0
+    d = Array.new(m+1) {Array.new(n+1)}
+
+    (0..m).each {|i| d[i][0] = i}
+    (0..n).each {|j| d[0][j] = j}
+    (1..n).each do |j|
+      (1..m).each do |i|
+        d[i][j] = if string[i-1] == text[j-1]  # adjust index into string
+                    d[i-1][j-1]       # no operation required
+                  else
+                    [ d[i-1][j]+1,    # deletion
+                      d[i][j-1]+1,    # insertion
+                      d[i-1][j-1]+1,  # substitution
+                    ].min
+                  end
+      end
+    end
+    d[m][n]
+  end
+
+  def self.find_closest(string)
+    results = []
+    closest_distance = 3
+    Lacquer.all.each do |lacquer|
+      distance = lacquer.levenshtein_distance(string)
+      if distance <= closest_distance
+        results << lacquer
+      end
+    end
+    results
+  end
+
   def color_tags
     colors = []
     @user_lacquers = UserLacquer.where(:lacquer_id => self.id)
