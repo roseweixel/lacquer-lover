8f5d81b8593e87ec9025918cf98c96e56b749e0a
molgin <molgin@users.noreply.github.com>
2015-03-22 20:15:16 -0400

    simple validation

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -14,6 +14,8 @@ class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
 
+  validates_presence_of :user, :friend
+
   validate :is_not_duplicate, :on => :create
   validate :is_not_self, :on => :create
   #validates_uniqueness_of :buddy_one, scope: :buddy_two
