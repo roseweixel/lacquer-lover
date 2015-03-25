8ece0740221e8eaf82dfb36b6a6f73e1056da136
molgin <molgin@users.noreply.github.com>
2015-03-25 11:04:12 -0400

    fix lacquer name validation

--- app/models/lacquer.rb
+++ app/models/lacquer.rb
@@ -26,7 +26,7 @@ class Lacquer < ActiveRecord::Base
 
   validates :name, :brand, presence: true, :on => :create
   validates_length_of :name, :minimum => 1
-  validates :name, uniqueness: true, scope: :brand
+  validates_uniqueness_of :name, scope: :brand
 
   accepts_nested_attributes_for :user_lacquers
   accepts_nested_attributes_for :swatches, :reject_if => proc { |attributes| attributes['image'].blank? }
