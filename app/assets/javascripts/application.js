22078f6fbd27d084d93e72d08cc1984b54196978
Rose Weixel <rose.weixel@gmail.com>
2015-03-29 22:04:42 -0400

    improve brand show page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,26 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require bootstrap-sprockets
+//= require_tree .
+
 (function() {
-    "use strict";
-    if (window.location && window.location.hash) {
-        if (window.location.hash === '#_=_') {
-            window.location.hash = '';
-            return;
-        }
-        var facebookFubarLoginHash = RegExp('_\=_', 'g');
-        window.location.hash = window.location.hash.replace(facebookFubarLoginHash,     '');
-    }
+  "use strict";
+
+  if (window.location.href.indexOf('#_=_') > 0) {
+    window.location = window.location.href.replace(/#.*/, '');
+  }
 }());
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,14 @@ class SessionsController < ApplicationController
         if request.env["HTTP_REFERER"].gsub(request.base_url, "") == root_path
           redirect_to user_path(user)
         else
-          binding.pry
+          #binding.pry
           redirect_to request.env["HTTP_REFERER"].gsub(request.base_url, "")
         end
       elsif request.env["action_dispatch.request.unsigned_session_cookie"] && request.env["action_dispatch.request.unsigned_session_cookie"]["omniauth.origin"]
         if request.env["action_dispatch.request.unsigned_session_cookie"]["omniauth.origin"].gsub(request.base_url, "") == root_path
           redirect_to user_path(user)
         else
-          binding.pry
+          #binding.pry
           redirect_to request.env["action_dispatch.request.unsigned_session_cookie"]["omniauth.origin"].gsub(request.base_url, "")
         end
       end
