706bfb1fa6f8fe790862508f39bfb2acd7c2cd38
Rose Weixel <rose.weixel@gmail.com>
2015-04-13 16:06:03 -0400

    work on mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "Lacquer Love&Lend <noreply@lacquer-love-and-lend.herokuapp.com>"
+  default from: "Lacquer Love&Lend <lacquerloveandlend@gmail.com>"
 
   # Emails to send:
 
@@ -7,7 +7,10 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     @signin_url = "http://lacquer-love-and-lend.herokuapp.com/auth/facebook"
-    mail(to: @user.email, subject: 'Welcome to Lacquer Love&Lend!', bcc: "lacquerloveandlend@gmail.com")
+    mail(to: @user.email, subject: 'Welcome to Lacquer Love&Lend!')
+
+    headers['X-MC-Track'] = "opens, clicks_all"
+    headers['X-MC-GoogleAnalytics'] = "yourdomain.com, yourotherdomain.com"
   end
 
   def invite_email(user, emails)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,12 +49,9 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
-  
+    address:   'smtp.mandrillapp.com',
+    port:      ENV["MANDRILL_PORT"],
+    user_name: ENV["MANDRILL_USERNAME"],
+    password:  ENV["MANDRILL_API_KEY"]
+  }
 end
