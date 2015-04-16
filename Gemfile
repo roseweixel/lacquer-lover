0e2e236e8588c5b35161aecc9ef7c588f938f251
Rose Weixel <rose.weixel@gmail.com>
2015-04-16 15:49:09 -0400

    horizontally scrolling image cards on lacquer show

--- Gemfile
+++ Gemfile
@@ -65,6 +65,7 @@ group :test, :development do
   # gem "sprockets_better_errors"
   gem "binding_of_caller"
   gem "factory_girl_rails"
+  gem "faker"
   #gem "simplecov"
   gem "database_cleaner"
   gem "sqlite3"
--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :review do
-    
+    rating { 1 + rand(5) }
+    comments { Faker::Lorem.sentences(5) }
+    user_id { User.all.pluck(:id).sample }
+    lacquer_id { Lacquer.all.pluck(:id).sample }
   end
-
 end
