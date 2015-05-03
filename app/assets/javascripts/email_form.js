10368970343b086d10d2129b6b32d0734b22f6c9
Rose Weixel <rose.weixel@gmail.com>
2015-05-03 02:08:07 -0400

    more work on about and contact

--- app/assets/javascripts/email_form.js
+++ app/assets/javascripts/email_form.js
@@ -1,10 +1,19 @@
 $(function(){
-  // $('#other_reply_address').hide();
   $('form').on('change', '#email-options', function(){
     if ($(this).val() === "other email address") {
       $('input#other_reply_address').show();
     } else {
       $('input#other_reply_address').hide();
     }
-  })
+  });
+
+  $('form').on('change', '#subject-options', function(){
+    if ($(this).val() === "other") {
+      $('input#other_subject').show();
+      $("input#other_subject").prop('required',true);
+    } else {
+      $('input#other_subject').hide();
+      $("input#other_subject").prop('required',false);
+    }
+  });
 })
