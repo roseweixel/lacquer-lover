c813c3dfe67e4f07860489c496e71420130c06dc
Rose Weixel <rose.weixel@gmail.com>
2015-05-06 22:03:10 -0400

    remove valid? method that was breaking lacquer creation

--- app/models/lacquer.rb
+++ app/models/lacquer.rb
@@ -41,30 +41,11 @@ class Lacquer < ActiveRecord::Base
     self.buy_url = base_url + query_string
   end
 
-  def valid?(url)
-    begin
-      if url.start_with?("lacquers/")
-        return false
-      elsif url.class == Paperclip::Attachment || !url.start_with?("http")
-        return true
-      else
-        uri = Addressable::URI.parse(url)
-
-        request = Net::HTTP.new uri.host
-
-        # rescue SocketError that occurs when offline
-        response= request.request_head uri.path
-
-        response.code.to_i == 200
-      end
-    rescue SocketError
-      return false
-    end
-  end
-
   def url_for_buy_it_link
-    if (Brand::BRANDS_WITH_ITEM_URL_AS_BUY_IT_URL.include? brand.name) && !!item_url && valid?(item_url)
-      item_url
+    if item_url
+      if (Brand::BRANDS_WITH_ITEM_URL_AS_BUY_IT_URL.include? brand.name)
+        item_url
+      end
     else
       buy_url
     end
